d55352cfbd58bcbf69e2b8caa44b66e161279c36
rootchack <rootchack@gmail.com>
2017-06-11 16:45:17 -0500

    Update bot.php

--- PHPbot/bot.php
+++ PHPbot/bot.php
@@ -1,16 +1,39 @@
 <?php
-
-$file = fopen("/db.txt", "r");
-
-$l1 = "";
-$input = "";
+$file = fopen("db.txt", "r");
+$linea =  fgets($file);
+$input = $_GET['comando'];
 $firstboot = TRUE;
 
 
 if ($firstboot == TRUE) {
-  echo "Hola, soy freebot, puedes hacerme cualquier pregunta.";
+  echo nl2br("El Bot libre!");
   $firstboot = FALSE;
 }
 
+list($pregunta, $respuesta) = explode('@', $linea);
+
+if ($input == $pregunta){
+	echo "<br>Tú: $input<br> $respuesta";
+	fclose($file);
+}
+elseif ($input != $pregunta) {
+
+	while (!feof($file)) { 
+
+        $linea = fgets($file); 
+        list($pregunta, $respuesta) = explode('@', $linea);
+
+        if ($input == $pregunta){
+	echo "<br>Tú: $input<br> $respuesta";
+	fclose($file);
+	break;
+ }
+}
+
+}
 
 ?>
+<form action="index.php" method="get">
+<input type="text" name="comando">
+<input type="submit" value="Enviar">
+</form> 
